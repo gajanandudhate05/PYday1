859a483f3c5ae9725dba98960d4512b7f15caf78
gajanandudhate05 <gbdudhate5102004@gmail.com>
2026-09-11 15:34:07 +0530

    monday

---
+++ monTosun.py
@@ -0,0 +1,15 @@
+a=int(input("enter a number for days(0-6):"))
+if a==0:
+    print("monday")
+elif a==1:
+    print("tuesday")
+elif a==2:
+    print("wenesday")
+elif a==3:
+    print("thursday")
+elif a==4:
+    print("friday")
+elif a==5:
+    print("saturday")
+elif a==6:
+    print("sunday")
